aea2a232a6e3bc8943e23afaef48b96041495373
654060747 <654060747@qq.com>
2018-10-30 00:07:15 +0800

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -28,3 +28,41 @@ for x in csv_file:
 					os.rename('./'+file_list+'/'+temp+'/'+one_file,'./'+file_list+'/'+temp+'/'+new_name+'.'+one_file.split('.')[1])
 				else:
 					os.rename('./'+file_list+'/'+temp+'/'+one_file,'./'+file_list+'/'+temp+'/'+new_name)
+
+					
+					
+					
+					
+					
+					
+					
+# coding = utf-8
+import os
+
+
+# 文件目录
+root_path = "./gege/" # csv.reader(open(csv_name,'r'))
+# 年与月之间需要修改的原名字
+original_name = "零拾"
+# 年与月之间修改后的名字
+make_name = "壹拾"
+
+
+
+file_name = os.listdir(root_path)
+# 遍历文件内容
+for name in file_name:
+	# print(x)
+	try:
+		sect_name = name.split("年")[1].split("月")[0]
+		print(sect_name)
+
+		if original_name in sect_name:
+
+			new_name = name.split("年")[0]+"年"+make_name+"月"+name.split("月")[1]
+			print(new_name)
+			os.rename(root_path+name, root_path+new_name)
+
+	except Exception as e:
+		print("====缺少年月===="+name)
+	
