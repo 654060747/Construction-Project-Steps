823d262ea780126a36f54fe5793752c52f6c0762
654060747 <654060747@qq.com>
2019-04-22 23:11:25 +0800

    睡眠一段时间python自动回收线程

--- read_csv.py
+++ read_csv.py
@@ -131,5 +131,28 @@ for i in range(s_id, e_id):
     thread.start()	
 		
 		
-		
+
+# 睡眠一段时间python自动回收线程
+# coding =utf-8
+import threading,time
+
+class MyThread(threading.Thread):
+    """docstring for MyThread"""
+    def __init__(self, arg):
+        super(MyThread, self).__init__()
+        self.arg = arg
+      
+    def run(self):
+        
+        print("开始下载图片==="+threading.current_thread().getName())
+        time.sleep(2)
+        print("OK==="+threading.current_thread().getName())
+
+i = 0
+while True:
+	i = i+1
+	thread = MyThread(str(i))
+	thread.start()
+	if i%100 == 0:
+		time.sleep(5)
 		
