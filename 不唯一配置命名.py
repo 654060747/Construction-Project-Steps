a8c9320e50dcf4cc32fc5cd15853660d4ee50a77
654060747 <654060747@qq.com>
2019-11-06 21:57:31 +0800

    前端提供不唯一文件分类与不分类img与json命名

--- 不唯一配置命名.py
+++ 不唯一配置命名.py
@@ -13,7 +13,7 @@ def file_name(file_dir):
 			if os.path.splitext(file)[1] == '.jpg':
 				jpg_list.append(os.path.splitext(file)[0])
 			elif os.path.splitext(file)[1] == '.json':
-				json_list.append(os.path.splitext(file)[0])
+				json_list.append(os.path.splitext(file)[0].split('.jpg')[0])
 		# print(jpg_list)
 		# print(json_list)
 
@@ -24,7 +24,7 @@ def file_name(file_dir):
 					i = i + 1
 					jpg_old_name = os.path.join(root,json_one+".jpg")
 					jpg_new_name = os.path.join(root,json_one.split("_")[0]+"_"+str(i)+".jpg")
-					json_old_name = os.path.join(root,json_one+".json")
+					json_old_name = os.path.join(root,json_one+".jpg.json")
 					json_new_name = os.path.join(root,json_one.split("_")[0]+"_"+str(i)+".json")
 
 					print(jpg_old_name)
@@ -37,6 +37,66 @@ def file_name(file_dir):
 				pass
 			pass
 
-file_name(file_dir)             
+file_name(file_dir)
+
+
+
+
+
+
+
+
+# -*- coding: utf-8 -*-
+import os,time,shutil
+
+# 目录结构(以下都在同级目录,脚本也放在此级目录)
+# lukou(所有路口放入此目录)
+# mark
+# mark1
+# json
+# 生成的ok目录位置与以上目录为同级目录
+
+
+file_dir_one = "./lukou/"
+mark1_path = "./mark1/"
+json_path = "./json/"
+
+ok_path = "./ok/"
+
+
+def CheckDir(dir):
+    if not os.path.exists(dir):
+      os.makedirs(dir)
+    pass
+
+ 
+def file_name():
+
+	one_list = {}
+	for root_one, dirs_one, files_one in os.walk(file_dir_one):
+		for d_file_one in files_one:
+			one_list[d_file_one] = root_one
+
+	for root_two, dirs_two, files_two in os.walk(mark1_path):
+		for d_file_two in files_two:
+			file_path = one_list[d_file_two].split('/')[-1]
+			if '-' in file_path:
+				img_name = file_path.split('-')[0]+"_"+file_path.split('-')[1]+".jpg"
+			else:
+				img_name = file_path+".jpg"
+			# 老地址老名字
+			old_img = root_two+"/"+d_file_two
+			old_json = json_path+"/"+d_file_two+".txt"
+			# 创建copy后的目录
+			new_img_path = ok_path+file_path+"/"
+			CheckDir(new_img_path)
+			# 新地址新名字
+			new_img = new_img_path+img_name
+			new_json = new_img_path+img_name.split(".jpg")[0]+".txt"
+			shutil.copy(old_img,new_img)
+			shutil.copy(old_json,new_json)
+
+file_name()             
+             
 
 
